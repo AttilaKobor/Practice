041fc2f6122f708ac8c93777402d98aff4efa62c
AttilaKobor <illatoztika@gmail.comgit config --global user.name AttilaKoborOmit --git config --global user.email illatoztika@gmail.com>
2018-08-14 20:11:10 +0200

    lines

--- Java/Print each line/src/Study/Main.java
+++ Java/Print each line/src/Study/Main.java
@@ -1,21 +1,27 @@
 package Study;
 
+import javax.sound.sampled.Line;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("my-file.txt");
         try {
-            System.out.println(Files.lines(path));
+            for (String line : Files.readAllLines(Paths.get("my-file.txt")))
+            {
+                System.out.println(line);
+            }
+
         } catch (NoSuchFileException e)
         {
-            System.out.println("Unable to read file: my-file.txt");
+            System.out.println("There is no such file!");
         }
+
 
     }
 }
