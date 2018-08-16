05a358dc3dab7d96a3c2e6106c13524007727f10
AttilaKobor <40570753+AttilaKobor@users.noreply.github.com>
2018-08-16 18:47:45 +0200

    Update Main.java

--- Java/Recursion/Counter/src/Recursion/Main.java
+++ Java/Recursion/Counter/src/Recursion/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
-        double n = scanner.nextDouble();
+        int n = scanner.nextDouble();
         System.out.println(Counter(n));
 
     }
 
-    private static double Counter(double n) {
+    private static int Counter(int n) {
         if (n > 0) {
             Counter(n-1);
             System.out.println(n);
